c37892dfd8546b2b9773dd4affbcb26a86fd650c
yanto-sugiyanto <71365660+yanto-sugiyanto@users.noreply.github.com>
2020-09-29 22:01:37 +0700

    Add files via upload

---
+++ precache-manifest.50b6d6c6c3e1509d46f311d7785a6ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5bceed0d32a7ff0759bd171cdb2162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b674c51739b4ae0f31b7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fe0f858930b2ba70b959",
+    "url": "/static/js/2.c20edc23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c20edc23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b674c51739b4ae0f31b7",
+    "url": "/static/js/main.836487b5.chunk.js"
+  },
+  {
+    "revision": "d7a6e5881c5d479b7594",
+    "url": "/static/js/runtime-main.2ea4cdbd.js"
+  }
+]);
